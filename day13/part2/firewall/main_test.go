379c4ceb8e346e84183b3d21dc7e367deda6f37b
José Ribeiro <hello@jlbribeiro.com>
2017-12-13 22:49:51 +0000

    Day 13/Part 2

---
+++ day13/part2/firewall/main_test.go
@@ -0,0 +1,56 @@
+package firewall_test
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/jlbribeiro/adventofcode/day13/part2/firewall"
+)
+
+type testLayer struct {
+	depth         int
+	scanningRange int
+}
+
+var undetectedWalkDelayTests = []struct {
+	layers   []testLayer
+	expected int
+}{
+	{
+		layers: []testLayer{
+			{
+				depth:         0,
+				scanningRange: 3,
+			},
+			{
+				depth:         1,
+				scanningRange: 2,
+			},
+			{
+				depth:         4,
+				scanningRange: 4,
+			},
+			{
+				depth:         6,
+				scanningRange: 4,
+			},
+		},
+		expected: 10,
+	},
+}
+
+func TestUndetectedWalkDelay(t *testing.T) {
+	for i, tt := range undetectedWalkDelayTests {
+		t.Run(fmt.Sprintf("firewall.UndetectedWalkDelay(test_i=%d)", i), func(t *testing.T) {
+			fw := firewall.NewFirewall()
+			for _, layer := range tt.layers {
+				fw.AddLayer(firewall.NewLayer(layer.depth, layer.scanningRange))
+			}
+
+			actual := fw.UndetectedWalkDelay()
+			if actual != tt.expected {
+				t.Errorf("firewall.UndetectedWalkDelay(test_i=%d): expected %d, got %d", i, tt.expected, actual)
+			}
+		})
+	}
+}

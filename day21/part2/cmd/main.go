8f13cb455bcbfc0f5fe08a5e02548305058059a6
José Ribeiro <hello@jlbribeiro.com>
2017-12-23 02:51:19 +0000

    Day 21/Part 2

---
+++ day21/part2/cmd/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+
+	"github.com/jlbribeiro/adventofcode/day21/part2/fractal"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	rules := []string(nil)
+	for scanner.Scan() {
+		rule := scanner.Text()
+		rules = append(rules, rule)
+	}
+
+	fractalGenerator := fractal.NewGeneratorFromInput(rules)
+	fractalGenerator.Run(18)
+	fmt.Println(fractalGenerator.MatrixOnPixels())
+}
